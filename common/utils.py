1b7145b8bb86e44313dcd1b265c3d8b5afee59f4
Katsuhiko Matsumoto <le-bleu@horae.dti.ne.jp>
2024-03-22 09:46:15 +0900

    new

---
+++ common/utils.py
@@ -0,0 +1,154 @@
+import tensorflow as tf
+
+import pathlib
+import time
+import datetime
+import sys
+import time
+import cv2
+import scipy.misc
+from skimage.transform import resize
+from PIL import Image
+import imageio
+import numpy as np
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+def load(image_file):
+  # Read and decode an image file to a uint8 tensor
+  image = tf.io.read_file(image_file)
+  image = tf.io.decode_jpeg(image)
+
+  # Split each image tensor into two tensors:
+  # - one with a real building facade image
+  # - one with an architecture label image
+  w = tf.shape(image)[1]
+  w = w // 2
+  input_image = image[:, w:, :]
+  real_image = image[:, :w, :]
+
+  # Convert both images to float32 tensors
+  input_image = tf.cast(input_image, tf.float32)
+  real_image = tf.cast(real_image, tf.float32)
+
+  return input_image, real_image
+
+def resize2(input_image, real_image, height, width):
+  input_image = tf.image.resize(input_image, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  real_image = tf.image.resize(real_image, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  return input_image, real_image
+
+
+def random_crop(input_image, real_image):
+    stacked_image = tf.stack([input_image, real_image], axis=0)
+    cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+
+    return cropped_image[0], cropped_image[1]
+
+def normalize(input_image, real_image):
+  input_image = (input_image / 127.5) - 1
+  real_image = (real_image / 127.5) - 1
+
+  return input_image, real_image
+
+@tf.function()
+def random_jitter(input_image, real_image):
+  # Resizing to 286x286
+  input_image, real_image = resize2(input_image, real_image, IMG_HEIGHT, IMG_WIDTH)
+
+  # Random cropping back to 256x256
+  input_image, real_image = random_crop(input_image, real_image)
+
+  if tf.random.uniform(()) > 0.5:
+    # Random mirroring
+    input_image = tf.image.flip_left_right(input_image)
+    real_image = tf.image.flip_left_right(real_image)
+
+  return input_image, real_image
+
+def load_image_train(image_file):
+  #print('image_file:',image_file)
+  input_image, real_image = load(image_file)
+  input_image, real_image = random_jitter(input_image, real_image)
+  input_image, real_image = normalize(input_image, real_image)
+  #print('input:',input_image)
+  #print('real:',real_image)
+  return input_image, real_image
+
+def load_image_test(image_file):
+  input_image, real_image = load(image_file)
+  input_image, real_image = resize2(input_image, real_image,
+                                   IMG_HEIGHT, IMG_WIDTH)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
+#----------------------------
+#The MIT License (MIT)
+#
+#Copyright (c) 2016 Taehoon Kim
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+def save_images(images, size, image_path):
+  return imsave(inverse_transform(images), size, image_path)
+
+def imsave(images, size, path):
+  image = np.squeeze(merge(images, size))
+  return imageio.imwrite(path, image)
+
+def image_manifold_size(num_images):
+  manifold_h = int(np.floor(np.sqrt(num_images)))
+  manifold_w = int(np.ceil(np.sqrt(num_images)))
+  assert manifold_h * manifold_w == num_images
+  return manifold_h, manifold_w
+
+def inverse_transform(images):
+  return (images+1.)/2.
+
+def merge(images, size):
+  h, w = images.shape[1], images.shape[2]
+  if (images.shape[3] in (3,4)):
+    c = images.shape[3]
+    img = np.zeros((h * size[0], w * size[1], c))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return img
+  elif images.shape[3]==1:
+    img = np.zeros((h * size[0], w * size[1]))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+    return img
+  else:
+    raise ValueError('in merge(images,size) images parameter '
+                     'must have dimensions: HxW or HxWx3 or HxWx4')
+
+def timestamp(s='%Y%m%d.%H%M%S', ts=None):
+  if not ts: ts = time.time()
+  st = datetime.datetime.fromtimestamp(ts).strftime(s)
+  return st
